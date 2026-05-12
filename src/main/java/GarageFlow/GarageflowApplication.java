0ac2e99664d78482e80cb70e820d7a6a8b4084bc
TanishaSharma <sharmatanisha94724@gmail.com>
2026-05-12 14:41:56 +0530

    Initial Spring Boot setup

---
+++ src/main/java/GarageFlow/GarageflowApplication.java
@@ -0,0 +1,13 @@
+package GarageFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GarageFlow/GarageflowApplicationTests.java
@@ -0,0 +1,13 @@
+package GarageFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
